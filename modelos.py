b63be40ef370978a42967ff149de0d5691d289a7
pedro.abreu <pedro.aurelio.abreu@gmail.com>
2023-12-15 11:21:52 -0300

    Develop 2D Solver

--- modelos.py
+++ modelos.py
@@ -1,18 +1,19 @@
 import numpy as np
 
 
-def solve_one_dimensional_diffusion_convection(material_props, mesh, west_bound,
+def solve_1d_diffusion_convection(material_props, mesh, west_bound,
                                                east_bound, external_convection=None, volumetric_source=None,
                                                velocity_field=None):
-    velocity_field, external_convection, volumetric_source = initialize_one_dimensional_solver(velocity_field,
+    velocity_field, external_convection, volumetric_source = initialize_1d_solver(velocity_field,
                                                                                                external_convection,
                                                                                                volumetric_source)
     n_x = int(mesh["L_x"] / mesh["d_x"])
 
-    thermal_resist, vol_heat_source, convection_resist, F_e, F_w = define_equation_components(n_x, mesh, material_props,
-                                                                                              volumetric_source,
-                                                                                              velocity_field,
-                                                                                              external_convection)
+    thermal_resist, vol_heat_source, convection_resist, F_e, F_w = define_1d_equation_components(n_x, mesh,
+                                                                                                              material_props,
+                                                                                                              volumetric_source,
+                                                                                                              velocity_field,
+                                                                                                              external_convection)
 
     A, b = np.zeros((n_x, n_x)), np.zeros(n_x)
 
@@ -43,7 +44,7 @@ def solve_one_dimensional_diffusion_convection(material_props, mesh, west_bound,
     return np.linalg.solve(A, b)
 
 
-def initialize_one_dimensional_solver(velocity_field, external_convection, volumetric_source):
+def initialize_1d_solver(velocity_field, external_convection, volumetric_source):
     if velocity_field is None:
         velocity_field = {"u": 0}
     if external_convection is None:
@@ -53,7 +54,7 @@ def initialize_one_dimensional_solver(velocity_field, external_convection, volum
     return velocity_field, external_convection, volumetric_source
 
 
-def define_equation_components(n_x, mesh, material_props, volumetric_source, velocity_field, external_convection):
+def define_1d_equation_components(n_x, mesh, material_props, volumetric_source, velocity_field, external_convection):
     thermal_resist = material_props["k"] * mesh["area"] / mesh["d_x"]
     vol_heat_source = volumetric_source["q_dot"] * mesh["area"] * mesh["d_x"]
     convection_resist = external_convection['h'] * external_convection['perimeter'] * mesh["d_x"]
@@ -62,3 +63,99 @@ def define_equation_components(n_x, mesh, material_props, volumetric_source, vel
     return thermal_resist, vol_heat_source, convection_resist, F_e, F_w
 
 
+
+
+
+
+
+
+
+
+def solve_2d_diffusion_convection(material_props, mesh, west_bound, north_bound, south_bound,
+                                               east_bound, external_convection=None, volumetric_source=None,
+                                               velocity_field=None):
+    velocity_field, external_convection, volumetric_source = initialize_2d_solver(velocity_field,
+                                                                                               external_convection,
+                                                                                               volumetric_source)
+    n_x = int(round(mesh["L_x"] / mesh["d_x"]))
+    n_y = int(round(mesh["L_y"] / mesh["d_x"]))
+
+    thermal_resist, vol_heat_source, convection_resist, F_e, F_w, F_n, F_s = define_2d_equation_components(mesh,
+                                                                                                              material_props,
+                                                                                                              volumetric_source,
+                                                                                                              velocity_field,
+                                                                                                              external_convection)
+
+    A, b = np.zeros((n_x*n_y, n_x*n_y)), np.zeros(n_x*n_y)
+
+    for i in range(n_x):
+        for j in range(n_y):
+            S_u = vol_heat_source + convection_resist * external_convection['T_conv']
+            S_p = -convection_resist
+            a_w = thermal_resist + np.max([F_w, 0])
+            a_e = thermal_resist + np.max([-F_e, 0])
+            a_s = thermal_resist + np.max([F_s, 0])
+            a_n = thermal_resist + np.max([-F_n, 0])
+            if i == 0:
+                a_w = 0
+                if west_bound['condition'] == 'dirichlet':
+                    S_p += -(2 * thermal_resist + np.max([F_w, 0]))
+                    S_u += (2 * thermal_resist + np.max([F_w, 0])) * west_bound['value']
+                elif (west_bound['condition'] == 'neumann') & (west_bound['value'] != 0):
+                    S_u += west_bound['value']*mesh['area']
+            if i == n_x - 1:
+                a_e = 0
+                if east_bound['condition'] == 'dirichlet':
+                    S_p += -(2 * thermal_resist + np.max([-F_e, 0]))
+                    S_u += (2 * thermal_resist + np.max([-F_e, 0])) * east_bound['value']
+                elif (east_bound['condition'] == 'neumann') & (east_bound['value'] != 0):
+                    S_u += east_bound['value'] * mesh['area']
+            if j == 0:
+                a_s = 0
+                if south_bound['condition'] == 'dirichlet':
+                    S_p += -(2 * thermal_resist + np.max([F_s, 0]))
+                    S_u += (2 * thermal_resist + np.max([F_s, 0])) * south_bound['value']
+                elif (south_bound['condition'] == 'neumann') & (south_bound['value'] != 0):
+                    S_u += south_bound['value']*mesh['area']
+
+            if j == n_y - 1:
+                a_n = 0
+                if north_bound['condition'] == 'dirichlet':
+                    S_p += -(2 * thermal_resist + np.max([-F_n, 0]))
+                    S_u += (2 * thermal_resist + np.max([-F_n, 0])) * north_bound['value']
+                elif (north_bound['condition'] == 'neumann') & (north_bound['value'] != 0):
+                    S_u += north_bound['value']*mesh['area']
+
+            if i != 0:
+                A[i + j*n_x, (i + j*n_x) - 1] = -a_w
+            if i != (n_x - 1):
+                A[i + j*n_x, (i + j*n_x) + 1] = -a_e
+            if j != 0:
+                A[i + j*n_x, (i + (j-1)*n_x)] = -a_s
+            if j != (n_y-1):
+                A[i + j * n_x, (i + (j + 1) * n_x)] = -a_n
+
+            A[i + j * n_x][i + j * n_x] = a_w + a_e + a_n + a_s - S_p + (F_e - F_w) + (F_n - F_s)
+            b[i + j * n_x] = S_u
+    return np.linalg.solve(A, b)
+
+
+def initialize_2d_solver(velocity_field, external_convection, volumetric_source):
+    if velocity_field is None:
+        velocity_field = {"u": 0, "v": 0}
+    if external_convection is None:
+        external_convection = {'perimeter': 0, 'h': 0, 'T_conv': 0}
+    if volumetric_source is None:
+        volumetric_source = {'q_dot': 0}
+    return velocity_field, external_convection, volumetric_source
+
+
+def define_2d_equation_components(mesh, material_props, volumetric_source, velocity_field, external_convection):
+    thermal_resist = material_props["k"] * mesh["area"] / mesh["d_x"]
+    vol_heat_source = volumetric_source["q_dot"] * mesh["area"] * mesh["d_x"]
+    convection_resist = external_convection['h'] * external_convection['perimeter'] * mesh["d_x"]
+    F_e = material_props["rho"] * velocity_field["u"] * mesh["area"]
+    F_w = material_props["rho"] * velocity_field["u"] * mesh["area"]
+    F_n = material_props["rho"] * velocity_field["v"] * mesh["area"]
+    F_s = material_props["rho"] * velocity_field["v"] * mesh["area"]
+    return thermal_resist, vol_heat_source, convection_resist, F_e, F_w, F_n, F_s
--- tests.py
+++ tests.py
@@ -15,7 +15,7 @@ class MyTestCase(unittest.TestCase):
         east_bound = {'condition': 'dirichlet', 'value': 500}
         material_props = {'k': 1000, 'rho':1}
         self.assertTrue(np.allclose(
-                        modelos.solve_one_dimensional_diffusion_convection(mesh=mesh, west_bound=west_bound,
+                        modelos.solve_1d_diffusion_convection(mesh=mesh, west_bound=west_bound,
                                                                 east_bound=east_bound,
                                                                 material_props=material_props),
                         np.array([140., 220., 300., 380., 460.])),
@@ -36,7 +36,7 @@ class MyTestCase(unittest.TestCase):
         volumetric_source = {"q_dot":1e06}
         material_props = {'k': 0.5,'rho': 1}
         self.assertTrue(np.allclose(
-                        modelos.solve_one_dimensional_diffusion_convection(mesh=mesh,
+                        modelos.solve_1d_diffusion_convection(mesh=mesh,
                                                                 west_bound=west_bound,
                                                                 east_bound=east_bound,
                                                                 material_props=material_props,
@@ -55,11 +55,11 @@ class MyTestCase(unittest.TestCase):
         # Solution for 5 cells
         mesh = {"L_x": 1, "d_x": 0.2, "area": 1}
         west_bound = {'condition': 'dirichlet', 'value': 100}
-        east_bound = {'condition': 'neumann'}
+        east_bound = {'condition': 'neumann', 'value': 0}
         material_props = {'k': 1, 'rho':1}
         external_convection = {'perimeter':1, 'h':25, 'T_conv':20}
         self.assertTrue(np.allclose(
-                        modelos.solve_one_dimensional_diffusion_convection(mesh=mesh,
+                        modelos.solve_1d_diffusion_convection(mesh=mesh,
                                                                 west_bound=west_bound,
                                                                 east_bound=east_bound,
                                                                 material_props=material_props,
@@ -67,7 +67,7 @@ class MyTestCase(unittest.TestCase):
                         np.array([64.22764228, 36.91056911, 26.50406504, 22.60162602, 21.30081301])),
                         'The diffusion case with external convection and neumann condition is not correctly solved')
 
-    def test_one_dimensional_diffusion_convection(self):
+    def test_1d_diffusion_convection(self):
         # A property φ is transported by means of convection and diffusion through the
         # one - dimensional domain sketched in Figure 5.2.The governing equation is (5.3);
         # the boundary conditions are φ(0) = 1 at x = 0 and φ(L) = 0 at x = L.Using five equally
@@ -81,13 +81,31 @@ class MyTestCase(unittest.TestCase):
         material_props = {'k': 0.1, 'rho': 1}
         velocity_field = {'u': 0.1}
         self.assertTrue(np.allclose(
-            modelos.solve_one_dimensional_diffusion_convection(mesh=mesh,
+            modelos.solve_1d_diffusion_convection(mesh=mesh,
                                                     west_bound=west_bound,
                                                     east_bound=east_bound,
                                                     material_props=material_props,
                                                     velocity_field=velocity_field),
             np.array([0.93373341, 0.7879469,  0.6130031,  0.40307053, 0.15115145])),
             'The convection diffusion case is not correctly solved')
+
+    def test_two_dimensional_diffusion_1(self):
+        material_props = {'k': 1000, 'rho': 1}
+        mesh = {"L_x": 0.3, "L_y": 0.4, "d_x": 0.1, "area": 1e-03}
+        west_bound = {'condition': 'neumann', 'value': 500000}
+        east_bound = {'condition': 'neumann', 'value': 0}
+        north_bound = {'condition': 'dirichlet', 'value': 100}
+        south_bound = {'condition': 'neumann', 'value': 0}
+        self.assertTrue(np.allclose(
+            modelos.solve_2d_diffusion_convection(material_props, mesh, west_bound, north_bound,
+                                                               south_bound,
+                                                               east_bound, external_convection=None,
+                                                               volumetric_source=None,
+                                                               velocity_field=None),
+            np.array([260.03673947, 227.79886148, 212.16439905, 242.27461747, 211.19544592,
+                     196.52993661, 205.591667,   178.17836812, 166.22996488, 146.32201542,
+                     129.69639469, 123.98158989])),
+            'The 2-D diffusion case is not correctly solved')
 
 
 if __name__ == '__main__':
