9c9ddaf782514cd2f0d95b0786f4b885002f9cb9
pedro.abreu <pedro.aurelio.abreu@gmail.com>
2023-12-15 09:17:18 -0300

    Distribut the solver in different functions

--- modelos.py
+++ modelos.py
@@ -4,20 +4,15 @@ import numpy as np
 def solve_one_dimensional_diffusion_convection(material_props, mesh, west_bound,
                                                east_bound, external_convection=None, volumetric_source=None,
                                                velocity_field=None):
-    if velocity_field is None:
-        velocity_field = {"u": 0}
-    if external_convection is None:
-        external_convection = {'perimeter': 0, 'h': 0, 'T_conv': 0}
-    if volumetric_source is None:
-        volumetric_source = {'q_dot': 0}
-
-
+    velocity_field, external_convection, volumetric_source = initialize_one_dimensional_solver(velocity_field,
+                                                                                               external_convection,
+                                                                                               volumetric_source)
     n_x = int(mesh["L_x"] / mesh["d_x"])
-    thermal_resist = material_props["k"] * mesh["area"] / mesh["d_x"]
-    vol_heat_source = volumetric_source["q_dot"] * mesh["area"] * mesh["d_x"]
-    convection_resist = external_convection['h'] * external_convection['perimeter'] * mesh["d_x"]
-    F_e = material_props["rho"] * velocity_field["u"] * mesh["area"]
-    F_w = material_props["rho"] * velocity_field["u"] * mesh["area"]
+
+    thermal_resist, vol_heat_source, convection_resist, F_e, F_w = define_equation_components(n_x, mesh, material_props,
+                                                                                              volumetric_source,
+                                                                                              velocity_field,
+                                                                                              external_convection)
 
     A, b = np.zeros((n_x, n_x)), np.zeros(n_x)
 
@@ -29,7 +24,7 @@ def solve_one_dimensional_diffusion_convection(material_props, mesh, west_bound,
         if i == 0:
             a_w = 0
             if west_bound['condition'] == 'dirichlet':
-                S_p += -(2*thermal_resist + np.max([F_w, 0]))
+                S_p += -(2 * thermal_resist + np.max([F_w, 0]))
                 S_u += (2 * thermal_resist + np.max([F_w, 0])) * west_bound['value']
         elif i == n_x - 1:
             a_e = 0
@@ -42,9 +37,28 @@ def solve_one_dimensional_diffusion_convection(material_props, mesh, west_bound,
         if i != (n_x - 1):
             A[i, i + 1] = -a_e
 
-        A[i][i] = a_w+a_e-S_p + (F_e-F_w)
+        A[i][i] = a_w + a_e - S_p + (F_e - F_w)
         b[i] = S_u
 
     return np.linalg.solve(A, b)
+
+
+def initialize_one_dimensional_solver(velocity_field, external_convection, volumetric_source):
+    if velocity_field is None:
+        velocity_field = {"u": 0}
+    if external_convection is None:
+        external_convection = {'perimeter': 0, 'h': 0, 'T_conv': 0}
+    if volumetric_source is None:
+        volumetric_source = {'q_dot': 0}
+    return velocity_field, external_convection, volumetric_source
+
+
+def define_equation_components(n_x, mesh, material_props, volumetric_source, velocity_field, external_convection):
+    thermal_resist = material_props["k"] * mesh["area"] / mesh["d_x"]
+    vol_heat_source = volumetric_source["q_dot"] * mesh["area"] * mesh["d_x"]
+    convection_resist = external_convection['h'] * external_convection['perimeter'] * mesh["d_x"]
+    F_e = material_props["rho"] * velocity_field["u"] * mesh["area"]
+    F_w = material_props["rho"] * velocity_field["u"] * mesh["area"]
+    return thermal_resist, vol_heat_source, convection_resist, F_e, F_w
 
 
